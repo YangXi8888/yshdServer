84b1bbb2c6622886d9d566faa4662b01c835078c
yangxi <yangxixi8888@126.com>
2016-05-31 19:27:50 +0800

    水水

--- doc/dc.bat
+++ doc/dc.bat
@@ -1,1 +1,1 @@
-expdp scott/wsbs@orcl schemas=scott dumpfile=yshd.dmp DIRECTORY=dpdata1  logfile=yshd.log
+expdp scott/wsbs@orcl schemas=scott dumpfile=yshd.dmp DIRECTORY=dpdata1  logfile=yshd.log version=10.2.0.1.0
