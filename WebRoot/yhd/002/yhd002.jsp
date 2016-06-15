9a13459c2b80116c8d86edf804f6bcfdb0560feb
DQ86 <duqing86@hotmail.com>
2016-06-16 00:34:47 +0800

    银行端上传文件

--- WebRoot/yhd/002/yhd002.jsp
+++ WebRoot/yhd/002/yhd002.jsp
@@ -6,38 +6,55 @@
 <meta http-equiv="cache-control" content="no-cache">
 <meta http-equiv="expires" content="0">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link rel="stylesheet" type="text/css" href="/public/jquery/themes/default/easyui.css"></link>
+<link rel="stylesheet" type="text/css" href="/public/jquery/themes/icon.css"></link>
+<link rel="stylesheet" type="text/css" href="/public/css/main.css"></link>
 
 <script type="text/javascript" src="/public/jquery/jquery-2.2.3.min.js"></script>
 <script type="text/javascript" src="/public/jquery/jquery.json.min.js"></script>
 <script type="text/javascript" src="/public/jquery/jquery.easyui.min.js"></script>
-<script type="text/javascript"
-	src="/public/jquery/locale/easyui-lang-zh_CN.js"></script>
+<script type="text/javascript" src="/public/jquery/locale/easyui-lang-zh_CN.js"></script>
 <script src="/public/jquery/plugins/upload/jquery.ajaxfileupload.js"></script>
+<script type="text/javascript" src="/public/jquery/plugins/download/jquery.fileDownload.js"></script>
+<script type="text/javascript" src="/public/My97DatePicker/WdatePicker.js"></script>
 <script type="text/javascript" src="/public/js/common.js"></script>
-<link rel="stylesheet" type="text/css"
-	href="/public/jquery/themes/default/easyui.css"></link>
-<link rel="stylesheet" type="text/css"
-	href="/public/jquery/themes/icon.css"></link>
-<link rel="stylesheet" type="text/css" href="/public/css/main.css"></link>
-<title></title>
+<title>泰州地税局税银互动平台</title>
 </head>
-<body>
-	<input type="file" name="uploadFiles[0].file">
-	<br>
-	<input type="file" name="uploadFiles[1].file">
-	<br>
-	<input type="file" name="uploadFiles[2].file">
-	<br>
-	<input type="file" name="uploadFiles[3].file">
-	<br>
-	<input type="file" name="uploadFiles[4].file">
-	<br>
-	<p>
-		<input type="button" value="上传" id="upBtn">
-	</p>
-	<input type="hidden" value="<%=request.getParameter("sessionId")%>"
-		id="sessionId">
+<body class="easyui-layout">
+	<div style="margin:0 auto; width:90%; height:90%; ">
 
+		<div class="easyui-panel" style="text-align: right; background-color: #E7F0FF;">
+			<a href="javascript:void(0);" class="easyui-linkbutton" data-options="plain:true,iconCls:'icon-search'" style="text-decoration: none;" onclick="queryData();">查询</a> <a href="javascript:void(0);" class="easyui-linkbutton" data-options="plain:true,iconCls:'icon-zip'" style="text-decoration: none;" id="upBtn">上传</a> <a href="#" class="easyui-linkbutton" data-options="plain:true,iconCls:'icon-close'" style="text-decoration: none;" onclick="window.close()">关闭</a>
+		</div>
+		<div style="text-align: center; height:10%"><br>
+			上传日期：<input class="Wdate" type="text" id="rqq" onClick="WdatePicker({maxDate:'#F{$dp.$D(\'rqz\')}'})" readonly="readonly"> 至<input class="Wdate" type="text" id="rqz" onClick="WdatePicker({minDate:'#F{$dp.$D(\'rqq\')}'})">
+		</div>
+		<div class="easyui-panel" title="上传记录" style="height:45%" id="dataTable"></div>
+		<div class="easyui-panel" title="文件上传" style="height:50%">
+			<table align="center" style="width:300px;height:260px;border:1pxsolid #95B8E7;">
+				<tr>
+					<td style="text-align: center;"><input type="file" name="uploadFiles[0].file"></td>
+				</tr>
+				<tr>
+					<td style="text-align: center;"><input type="file" name="uploadFiles[1].file"></td>
+				</tr>
+				<tr>
+					<td style="text-align: center;"><input type="file" name="uploadFiles[2].file"></td>
+				</tr>
+				<tr>
+					<td style="text-align: center;"><input type="file" name="uploadFiles[3].file"></td>
+				</tr>
+				<tr>
+					<td style="text-align: center;"><input type="file" name="uploadFiles[4].file"></td>
+				</tr>
+				<tr>
+					<td style="text-align: center;"><input type="file" name="uploadFiles[5].file"></td>
+				</tr>
+			</table>
+		</div>
+
+		<input type="hidden" value="<%=request.getParameter("sessionId")%>" id="sessionId">
+	</div>
 </body>
-<script src="/yhd/002/yhd002.js"></script>
+<script type="text/javascript" src="/yhd/002/yhd002.js"></script>
 </html>
--- biz/src/gov/jslt/taxcore/taxblh/yhd/yhd002/Yhd002BLH.java
+++ biz/src/gov/jslt/taxcore/taxblh/yhd/yhd002/Yhd002BLH.java
@@ -1,33 +1,148 @@
 package gov.jslt.taxcore.taxblh.yhd.yhd002;
 
+import gov.jslt.taxcore.taxblh.comm.CoreHelper;
+import gov.jslt.taxcore.taxblh.comm.FileTool;
+import gov.jslt.taxcore.taxbpo.comm.YhscjlbBPO;
+import gov.jslt.taxcore.taxbpo.comm.YhscjlbVO;
+import gov.jslt.taxevent.comm.FileVO;
+import gov.jslt.taxevent.comm.GeneralCons;
+import gov.jslt.taxevent.comm.JsonReqData;
 import gov.jslt.taxevent.comm.UploadFile;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import sun.jdbc.rowset.CachedRowSet;
 
 import com.ctp.core.blh.BaseBizLogicHandler;
+import com.ctp.core.bpo.QueryBPO;
 import com.ctp.core.event.RequestEvent;
 import com.ctp.core.event.ResponseEvent;
 import com.ctp.core.exception.TaxBaseBizException;
 
 public class Yhd002BLH extends BaseBizLogicHandler {
 
-	protected ResponseEvent performTask(RequestEvent req, Connection conn)
+	protected ResponseEvent performTask(RequestEvent reqEvent, Connection conn)
 			throws SQLException, TaxBaseBizException {
-		if ("upLoadFile".equals(req.getDealMethod())) {
-			return upLoadFile(req, conn);
+		if ("upLoadFile".equals(reqEvent.getDealMethod())) {
+			return upLoadFile(reqEvent, conn);
+		} else if ("queryData".equals(reqEvent.getDealMethod())) {
+			return queryData(reqEvent, conn);
+		} else if ("downLoadFile".equals(reqEvent.getDealMethod())) {
+			return downLoadFile(reqEvent, conn);
+		} else if ("deleteFile".equals(reqEvent.getDealMethod())) {
+			return deleteFile(reqEvent, conn);
 		}
 		return null;
 	}
 
-	protected ResponseEvent upLoadFile(RequestEvent req, Connection conn)
+	protected ResponseEvent queryData(RequestEvent reqEvent, Connection conn)
 			throws SQLException, TaxBaseBizException {
 		ResponseEvent responseEvent = new ResponseEvent();
-		List<UploadFile> uploadFiles = (List<UploadFile>) req.reqMapParam
+		JsonReqData reqData = (JsonReqData) reqEvent.reqMapParam
+				.get("JsonReqData");
+		ArrayList<Object> sqlParams = new ArrayList<Object>();
+		sqlParams.add(reqData.getYhwybz());
+		sqlParams.add(reqData.getData().get("rqq"));
+		sqlParams.add(reqData.getData().get("rqz"));
+		String sql = "SELECT T.SCJL_ID,\n"
+				+ "       B.XM,\n"
+				+ "       T.WJM,\n"
+				+ "       T.WJDX,\n"
+				+ "       TO_CHAR(T.LR_SJ, 'YYYY-MM-DD HH24:MI:SS') SCRQ\n"
+				+ "  FROM T_YS_YHSCJLB T, T_YS_LOGIN B\n"
+				+ " WHERE T.UUID = B.UUID\n"
+				+ "   AND T.UUID = ?\n"
+				+ "   AND T.LR_SJ > TO_DATE(? || ' 00:00:01', 'YYYY-MM-DD HH24:MI:SS')\n"
+				+ "   AND T.LR_SJ < TO_DATE(? || ' 23:59:59', 'YYYY-MM-DD HH24:MI:SS')";
+
+		CachedRowSet rs = QueryBPO.findAll(conn, sql, sqlParams);
+		List<Map<String, String>> dataList = new ArrayList<Map<String, String>>();
+		Map<String, String> dataMap;
+		while (rs.next()) {
+			dataMap = new HashMap<String, String>();
+			dataMap.put("SCJL_ID", rs.getString("SCJL_ID"));
+			dataMap.put("XM", rs.getString("XM"));
+			dataMap.put("WJM", rs.getString("WJM"));
+			dataMap.put("WJDX",
+					FileTool.bytes2kb(Long.decode(rs.getString("WJDX"))));
+			dataMap.put("SCRQ", rs.getString("SCRQ"));
+			dataList.add(dataMap);
+		}
+		responseEvent.respMapParam.put("dataList", dataList);
+		responseEvent.setReponseMesg("查询成功");
+		return responseEvent;
+	}
+
+	protected ResponseEvent deleteFile(RequestEvent reqEvent, Connection conn)
+			throws SQLException, TaxBaseBizException {
+		ResponseEvent responseEvent = new ResponseEvent();
+		JsonReqData reqData = (JsonReqData) reqEvent.reqMapParam
+				.get("JsonReqData");
+		String scjlId = String.valueOf(reqData.getData().get("scjlId"));
+		YhscjlbVO yhscjlbVO = YhscjlbBPO.queryByPK(conn, scjlId);
+		if (null == yhscjlbVO) {
+			responseEvent.setRepCode("ZB0004");
+			responseEvent.setReponseMesg(CoreHelper.getJyztMc(conn, "ZB0004"));
+			return responseEvent;
+		}
+		YhscjlbBPO.deleteByPK(conn, scjlId);
+		responseEvent.setReponseMesg("删除成功");
+		return responseEvent;
+	}
+
+	protected ResponseEvent downLoadFile(RequestEvent reqEvent, Connection conn)
+			throws SQLException, TaxBaseBizException {
+		ResponseEvent responseEvent = new ResponseEvent();
+		JsonReqData reqData = (JsonReqData) reqEvent.reqMapParam
+				.get("JsonReqData");
+		YhscjlbVO yhscjlbVO = YhscjlbBPO.queryByPK(conn, (String) reqData
+				.getData().get("scjlId"));
+		if (null != yhscjlbVO) {
+			FileVO fileVO = new FileVO();
+			fileVO.setFileContent(yhscjlbVO.getWjnrByte());
+			fileVO.setFileName(yhscjlbVO.getWjm());
+			fileVO.setFileType(yhscjlbVO.getWjm().substring(
+					yhscjlbVO.getWjm().lastIndexOf("."),
+					yhscjlbVO.getWjm().length()));
+			responseEvent.respMapParam.put(GeneralCons.FILE_VO, fileVO);
+			responseEvent.setReponseMesg("下载单个文件成功");
+		} else {
+			responseEvent.setRepCode("ZB0004");
+			responseEvent.setReponseMesg(CoreHelper.getJyztMc(conn, "ZB0004"));
+		}
+		return responseEvent;
+	}
+
+	protected ResponseEvent upLoadFile(RequestEvent reqEvent, Connection conn)
+			throws SQLException, TaxBaseBizException {
+		ResponseEvent responseEvent = new ResponseEvent();
+		JsonReqData jsonReqData = (JsonReqData) reqEvent.reqMapParam
+				.get("JsonReqData");
+		List<UploadFile> uploadFiles = (List<UploadFile>) reqEvent.reqMapParam
 				.get("UploadFiles");
-		System.out.println(uploadFiles.get(0).getFile().getFileName());
-		System.out.println(uploadFiles.get(0).getFile().getFileSize());
+		if (null == uploadFiles || uploadFiles.size() == 0) {
+			responseEvent.setRepCode("ZB0005");
+			responseEvent.setReponseMesg(CoreHelper.getJyztMc(conn, "ZB0005"));
+			return responseEvent;
+		}
+		YhscjlbVO yhscjlbVO = null;
+		for (int i = 0; i < uploadFiles.size(); i++) {
+			yhscjlbVO = new YhscjlbVO();
+			yhscjlbVO.setScjlid(CoreHelper.getGUID(conn));
+			yhscjlbVO.setUuid(jsonReqData.getYhwybz());
+			yhscjlbVO.setWjm(uploadFiles.get(i).getFile().getFileName());
+			yhscjlbVO.setWjdx(String.valueOf(uploadFiles.get(i).getFile()
+					.getFileSize()));
+			yhscjlbVO.setWjnrByte(FileTool.getFileContent(uploadFiles.get(i)
+					.getFile()));
+			YhscjlbBPO.insert(conn, yhscjlbVO);
+		}
+		responseEvent.setReponseMesg("上传成功");
 		return responseEvent;
 	}
 
